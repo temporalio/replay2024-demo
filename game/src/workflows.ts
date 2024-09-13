122becf5ee68b5b18fd4893ec9d931717e7f1a3a
Rob Holland <rob@temporal.io>
2024-09-13 11:43:13 +0100

    Give the tablet more time.

--- game/src/workflows.ts
+++ game/src/workflows.ts
@@ -506,7 +506,7 @@ async function startSnakes(config: GameConfig, snakes: Snakes) {
     startChild(SnakeWorkflow, {
       workflowId: snake.id,
       taskQueue: 'snakes',
-      workflowTaskTimeout: 500,
+      workflowTaskTimeout: '1 second',
       args: [{
         roundId: ROUND_WF_ID,
         id: snake.id,
