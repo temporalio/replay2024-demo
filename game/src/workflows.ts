2a821a7a749a2c8e95f5dc65e4e57d272bf39bff
Steve Androulakis <steve.androulakis@gmail.com>
2024-09-15 20:57:27 -0700

    added worker downtime to deploy, 10s

--- game/src/workflows.ts
+++ game/src/workflows.ts
@@ -24,7 +24,7 @@ import { GameConfig, Game, Teams, Round, Snake, Snakes, Direction, Point, Segmen
 const ROUND_WF_ID = 'SnakeGameRound';
 const APPLE_POINTS = 10;
 const SNAKE_MOVES_BEFORE_CAN = 20;
-const SNAKE_WORKER_DOWN_TIME = '5 seconds';
+const SNAKE_WORKER_DOWN_TIME = '10 seconds';
 
 const { emit } = proxyLocalActivities<ReturnType<typeof buildGameActivities>>({
   startToCloseTimeout: '1 seconds',
